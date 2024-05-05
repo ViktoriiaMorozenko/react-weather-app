e9fa8c4cfdc866684827981b694dd6eff1c57263
ViktoriiaMorozenko <113129727+ViktoriiaMorozenko@users.noreply.github.com>
2024-05-05 20:52:33 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-
+      <h1>Weather App</h1>
+      <footer>
+        The progect was coded by{" "}
+        <a href="https://github.com/ViktoriiaMorozenko" target="_blank">
+          Viktoriia Morozenko
+        </a>
+        {" "}
+        and is on{" "}
+        <a
+          href="https://github.com/ViktoriiaMorozenko/SheCodesResponsive_project"
+          target="_blank">
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://portfollio-viktoriiamorozenko.netlify.app/"
+          target="_blank">
+          {" "}
+          Netify
+        </a>
+      </footer>
     </div>
   );
 }
