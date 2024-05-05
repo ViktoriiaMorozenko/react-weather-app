b49ba5b6e5276d7780d438552f1e217ca5537616
ViktoriiaMorozenko <113129727+ViktoriiaMorozenko@users.noreply.github.com>
2024-05-05 21:14:48 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <button className="container btn btn-primary">Hello</button>
       <footer>
         The progect was coded by{" "}
-        <a href="https://github.com/ViktoriiaMorozenko" target="_blank">
+        <a
+          href="https://github.com/ViktoriiaMorozenko"
+          target="_blank"
+          rel="noreferrer">
           Viktoriia Morozenko
-        </a>
-        {" "}
+        </a>{" "}
         and is on{" "}
         <a
           href="https://github.com/ViktoriiaMorozenko/SheCodesResponsive_project"
-          target="_blank">
+          target="_blank"
+          rel="noreferrer">
           GitHub
         </a>{" "}
         and hosted on{" "}
         <a
           href="https://portfollio-viktoriiamorozenko.netlify.app/"
-          target="_blank">
+          target="_blank"
+          rel="noreferrer">
           {" "}
           Netify
         </a>
