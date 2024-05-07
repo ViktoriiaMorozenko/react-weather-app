19c4154c2e9ebb9d70f0483aa8182bcd2c60b731
ViktoriiaMorozenko <113129727+ViktoriiaMorozenko@users.noreply.github.com>
2024-05-07 21:36:55 +0100

    improved Unit Conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -12,15 +12,12 @@ function showCelsius(event){
     setUnit("celsius");
 }
 
-function fahrenheit() {
-  return (props.celsius * 9) / 5 + 32;
-}
-
 if (unit === "celsius") {
   return (
     <div className="WeatherTemperature">
       <span className="temperature">{Math.round(props.celsius)}</span>
       <span className="unit">
+        {" "}
         °C |{" "}
         <a href="/" onClick={showFahrenheit}>
           °F
@@ -28,10 +25,11 @@ if (unit === "celsius") {
       </span>
     </div>
   );
-} else {
+} else {  
+  let fahrenheit = (props.celsius * 9) / 5 + 32;
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.fahrenheit)}</span>
+        <span className="temperature">{Math.round(fahrenheit)}</span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
             {" "}
